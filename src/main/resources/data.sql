ce0dd1e42735b015eb3087f493768cb0949891f3
shimon0 <shimon.panipani36s@gmail.com>
2022-05-11 11:30:15 +0900

    DB設定

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-/* ユーザー マスター テスト用アカウント*/
-INSERT INTO users(
-    password,user_name,email,password,joining_day,age,gender,engineer_kinds,experience,introduction
-    )VALUES
-     ('システム管理者','system@co.jp','password','2000-01-01',21,1,1,10,'テスト用アカウントです');
--- src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-/* ユーザー マスタ */
-CREATE TABLE IF NOT EXISTS users
-    (user_id VARCHAR(50) PRIMARY KEY,user_name VARCHAR(50) NOT null,email VARCHAR(100) NOT null,password VARCHAR(100),
-     joining_date DATE NOT null,age INT ,gender INT,engineer_kinds VARCHAR(20),experience VARCHAR(20),introduction VARCHAR(250)
-    );
